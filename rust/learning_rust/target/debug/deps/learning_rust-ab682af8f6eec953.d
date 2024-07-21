e88f55298f27e6fac15bf1b502582f131eed97b2
dyang438 <152822883+dyang438@users.noreply.github.com>
2024-07-20 17:42:41 -0700

    new folders for zig and rust

---
+++ rust/learning_rust/target/debug/deps/learning_rust-ab682af8f6eec953.d
@@ -0,0 +1,5 @@
+/Users/davidyang438/code/Personal/blog_investigations/rust/learning_rust/target/debug/deps/liblearning_rust-ab682af8f6eec953.rmeta: src/main.rs
+
+/Users/davidyang438/code/Personal/blog_investigations/rust/learning_rust/target/debug/deps/learning_rust-ab682af8f6eec953.d: src/main.rs
+
+src/main.rs:
---
+++ rust/learning_rust/target/debug/deps/learning_rust-f5f3bd8b1a45bfdc.d
@@ -0,0 +1,5 @@
+/Users/davidyang438/code/Personal/blog_investigations/rust/learning_rust/target/debug/deps/liblearning_rust-f5f3bd8b1a45bfdc.rmeta: src/main.rs
+
+/Users/davidyang438/code/Personal/blog_investigations/rust/learning_rust/target/debug/deps/learning_rust-f5f3bd8b1a45bfdc.d: src/main.rs
+
+src/main.rs:
