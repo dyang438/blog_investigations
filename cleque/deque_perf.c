6575b15d600c46c8f09db5802472a1742f84cb2b
David Yang <davidyang438@gmail.com>
2024-07-20 17:44:57 -0700

    frag

--- cleque/deque_perf.c
+++ cleque/deque_perf.c
@@ -6,10 +6,12 @@
 #define NO_PRINT 0
 
 int pointer_bench();
+int fragmented_bench();
 
 int main ()
 {
-	pointer_bench();
+//	pointer_bench();
+	fragmented_bench();
 }
 
 
@@ -18,15 +20,39 @@ int pointer_bench ()
 	deque* main_deque = init_deque();
 	for (int i = 0; i < TEST_SIZE; i++) 
 	{
-		// push i onto the deque 1000 times, 
+		// push i onto the deque 10 times, 
 		deque_push_back(main_deque, i, PRINT);
 	}
+	fprintf(stderr, "\n");
 	for (int i = 0; i < MOVING_SIZE; i++)
 	{
 		//pop the front and push the back
-		int popped = deque_pop_front(main_deque);
-		deque_push_back(main_deque, popped, PRINT);
+		int popped_front = deque_pop_front(main_deque);
+		deque_push_back(main_deque, popped_front, PRINT);
 	}
 	free_deque(main_deque);
+	return 0;
+}
+
+
+int fragmented_bench() 
+{
+	deque* main_deque = init_deque();
+	void** frag_arr [2 * TEST_SIZE];
+	for (int i = 0; i < TEST_SIZE; i++)
+	{
+		deque_push_front(main_deque, i, PRINT);
+		void* frag1 = malloc(100);
+		frag_arr[2*i] = frag1;
+		
+		deque_push_back(main_deque, i, PRINT);
+		void* frag2 = malloc(100);
+		frag_arr[2*i+1] = frag2;
+	}
+
+	for (int i = 0; i < 2 * TEST_SIZE; i++) 
+	{
+		free(frag_arr[i]);
+	}
 	return 0;
 }
