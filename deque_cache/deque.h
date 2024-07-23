288f4f19c52d06f02ef4ea7b6c1b6077891de6a8
David Yang <davidyang438@gmail.com>
2024-07-22 20:22:51 -0700

    cleaning changes in preparation for post release

---
+++ deque_cache/deque.h
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+
+typedef struct deque_node 
+{
+	int payload;
+	struct deque_node* next;
+	struct deque_node* prev;
+} 
+deque_node;
+
+
+typedef struct deque
+{
+	struct deque_node* first;
+	struct deque_node* last;
+	int length;
+}
+deque;
+
+
+deque* init_deque ();
+
+int deque_pop_front (deque* input);
+
+int deque_pop_back (deque* input);
+
+void deque_push_front(deque *input, int val, int print);
+
+void deque_push_back(deque *input, int val, int print);
+
+void print_deque(deque* input);
+
+void free_deque(deque* input);
---
+++ deque_cache/deque_test_moving.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include "deque.h"
+#include "../time_bench/test_timing.h"
+
+#define TEST_SIZE 10
+#define MOVING_SIZE 10
+
+int test_one (deque* main_deque);
+
+int test_two (deque* main_deque);
+
+int test_three (deque* main_deque);
+
+int test_four (deque* main_deque);
+
+int main () 
+{
+	fprintf(stderr, "Starting moving test with TEST_SIZE = %d and MOVING_SIZE = %d\n", TEST_SIZE, MOVING_SIZE);
+
+	deque* deque_1 = init_deque();
+	test_one(deque_1);
+	free_deque(deque_1);
+
+	deque* deque_2 = init_deque();
+	test_two(deque_2);
+	free_deque(deque_2);
+
+	deque* deque_3 = init_deque();
+	test_three(deque_3);
+	free_deque(deque_3);
+
+	deque* deque_4 = init_deque();
+	test_four(deque_4);
+	free_deque(deque_4);
+}
+
+
+int test_one (deque* main_deque) 
+{
+	char* test_name = "Deque Test Push_front Pop_Front";
+
+	for (int i = 0; i < MOVING_SIZE; i++) 
+	{ 
+		deque_push_back(main_deque, i, 1);
+	}
+
+	fprintf(stderr, "length %d\n", main_deque->length);
+
+	clock_t start_time = timer_start_time(test_name);
+
+	for (int i = 0; i < TEST_SIZE; i++)
+	{
+		deque_push_front(main_deque, i, 1);
+		deque_pop_front(main_deque);
+	}
+
+	timer_end_time (test_name, start_time);
+
+	fprintf(stderr, "length %d\n", main_deque->length);
+
+	if (main_deque->length == MOVING_SIZE)
+	{
+		fprintf(stderr, "%s: Passed!\n", test_name);
+	}
+	else fprintf(stderr, "%s: Failed\n", test_name);
+
+	return 0;
+}
+
+
+int test_two (deque* main_deque) 
+{
+	char* test_name = "Deque Test Push_back Pop_Front";
+
+
+	for (int i = 0; i < MOVING_SIZE; i++) 
+	{ 
+		deque_push_back(main_deque, i, 0);
+	}
+
+	clock_t start_time = timer_start_time(test_name);
+
+	for (int i = 0; i < TEST_SIZE-3; i++)
+	{
+		deque_push_back(main_deque, i, 1);
+		deque_pop_front(main_deque);
+	}
+
+
+	timer_end_time (test_name, start_time);
+
+	if (main_deque->length == MOVING_SIZE) 
+	{
+		fprintf(stderr, "%s: Passed!\n", test_name);
+	}
+	else fprintf(stderr, "%s: Failed\n", test_name);
+
+	return 0;
+}
+
+
+int test_three (deque* main_deque) 
+{
+	char* test_name = "Deque Test Push_front Pop_back";
+
+
+	for (int i = 0; i < MOVING_SIZE; i++) 
+	{ 
+		deque_push_back(main_deque, i, 0);
+	}
+
+	clock_t start_time = timer_start_time(test_name);
+
+	for (int i = 0; i < TEST_SIZE-3; i++)
+	{
+		deque_push_front(main_deque, i, 1);
+		deque_pop_back(main_deque);
+	}
+
+
+	timer_end_time (test_name, start_time);
+
+	if (main_deque->length == MOVING_SIZE) 
+	{
+		fprintf(stderr, "%s: Passed!\n", test_name);
+	}
+	else fprintf(stderr, "%s: Failed\n", test_name);
+
+	return 0;
+}
+
+
+int test_four (deque* main_deque) 
+{
+	char* test_name = "Deque Test Push_back Pop_back";
+
+	for (int i = 0; i < MOVING_SIZE; i++) 
+	{ 
+		deque_push_back(main_deque, i, 0);
+	}
+
+	clock_t start_time = timer_start_time(test_name);
+	
+	for (int i = 0; i < TEST_SIZE-3; i++)
+	{
+		deque_push_back(main_deque, i, 1);
+		deque_pop_back(main_deque);
+	}
+
+
+	timer_end_time (test_name, start_time);
+
+	if (main_deque->length == MOVING_SIZE) 
+	{
+		fprintf(stderr, "%s: Passed!\n", test_name);
+	}
+	else fprintf(stderr, "%s: Failed\n", test_name);
+
+	return 0;
+}
+
