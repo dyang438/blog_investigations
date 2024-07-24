d9f2b2baebd90ff32dca820ab39d19ee1ed611de
David Yang <davidyang438@gmail.com>
2024-07-23 17:18:00 -0700

    demonstration finished!

--- vector_vs_ll/main.cpp
+++ vector_vs_ll/main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 #include <list>
 #include <random>
 #include <ctime>
@@ -13,28 +14,36 @@ std::uniform_int_distribution<rng_type::result_type> udist(0, 25);
 
 rng_type rng;
 
-void test_vec ();
-void test_list ();
-void test_deque ();
+void test_vec(double* times);
+void test_list(double* times);
+void test_deque(double* times);
 
 void add_sorted_vec(std::vector<int>& sorted_vec, int val);
 void add_sorted_list(std::list<int>& sorted_list, int val);
 void add_sorted_c_deque(deque* input, int val);
 
+
 int main() {
 	//seeding random number generator
 	rng_type::result_type const seed_val = static_cast<rng_type::result_type>(std::time(0));
-
 	rng.seed(seed_val);
 
-	
+	double times [3] = {0, 0, 0};
 
+	test_vec(times);
+	test_list(times);
+	
+	/* If you would like to see how fast the deque from that post runs compared to STL containers, it's here. It's basically implemented how std::list is */
+	//test_deque(times);
+
+
+	std::cout<<times[0]<<"\t"<<times[1]<<"\t"<<times[2]<<"\t";
 
 	return 2;
 }
 
 
-void test_vec () {
+void test_vec (double* times) {
 	std::vector<int> sorted_vec;
 	std::string vector_test = "Vector Test";
 
@@ -45,11 +54,17 @@ void test_vec () {
 		add_sorted_vec(sorted_vec, static_cast<int>(rand_num));
 	}	
 
-	timer_end_time(vector_test, start_time);
+	double time = timer_end_time(vector_test, start_time);
+
+	/*for (int i = 0; i < TEST_SIZE; i++) {*/
+	/*	std::cout<<sorted_vec[i]<<",";*/
+	/*}*/
+
+	times[0] = time;
 }
 
 
-void test_list () {
+void test_list (double* times) {
 	std::list<int> sorted_list;
 	std::string list_test = "List Test";
 
@@ -60,11 +75,18 @@ void test_list () {
 		add_sorted_list(sorted_list, static_cast<int>(rand_num));
 	}	
 
-	timer_end_time(list_test, start_time);
+	double time = timer_end_time(list_test, start_time);
+
+	/*for (auto& ele : sorted_list) {*/
+	/*	std::cout<<ele<<",";*/
+	/*}*/
+
+	times[1] = time;
+
 }
 
 
-void test_deque () {
+void test_deque (double* times) {
 	deque* sorted_deque = init_deque();
 	std::string deque_test = "Deque Test";
 
@@ -75,7 +97,12 @@ void test_deque () {
 		add_sorted_c_deque(sorted_deque, static_cast<int>(rand_num));
 	}	
 
-	timer_end_time(deque_test, start_time);
+	double time = timer_end_time(deque_test, start_time);
+	times[2] = time;
+
+	/*print_deque(sorted_deque);*/
+
+	free_deque(sorted_deque);
 }
 
 
@@ -105,13 +132,20 @@ void add_sorted_list(std::list<int>& sorted_list, int val) {
 }	
 
 
-void add_sorted_c_deque(deque* input, int val) {
-	deque_node* iter = input->first;
-	if (iter->payload > val) {
-		deque_push_front(input, val, 0);
+void add_sorted_c_deque(deque* sorted_deque, int val) {
+	if (!sorted_deque->first) {
+		deque_push_front(sorted_deque, val, 0);
+		return;
 	}
-	iter = iter->next;
-	while (iter->next != input->first) {
+
+	deque_node* iter = sorted_deque->first;
+
+	if (iter->payload > val) {
+		deque_push_front(sorted_deque, val, 0);
+		return;
+	} 
+
+	while (iter->next != sorted_deque->first) {
 		if (iter->payload > val) {
 			deque_node* pushed = (deque_node*)malloc(sizeof(deque_node));
 			pushed->next = iter;
@@ -121,8 +155,18 @@ void add_sorted_c_deque(deque* input, int val) {
 			pushed->payload = val;
 			return;
 		}
+		iter = iter->next;
 	}
-	deque_push_back(input, val, 0);
+	if (iter->payload > val) {
+		deque_node* pushed = (deque_node*)malloc(sizeof(deque_node));
+		pushed->next = iter;
+		pushed->prev = iter->prev;
+		pushed->prev->next = pushed;
+		iter->prev = pushed;
+		pushed->payload = val;
+		return;
+	}
+	deque_push_back(sorted_deque, val, 0);
 }
 
 
